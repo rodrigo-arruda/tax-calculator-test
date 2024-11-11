e54ffe5dcbfb3e98a95eced8e8bc16c174f6c39c
Rodrigo Arruda <rodrigo.arruda0684@gmail.com>
2024-11-10 22:33:01 -0500

    Rem unused code

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,17 +3,6 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:5001';
 const API_URL = `${BASE_URL}/tax-calculator/tax-year/`;
 
-// TODO: USE IT
-export const fetchTaxRates = async (year: number) => {
-  try {
-    const response = await axios.get(`${API_URL}${year}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching tax rates:', error);
-    throw new Error('Failed to fetch tax rates. Please try again later.');
-  }
-};
-
 export const fetchTaxRateByYear = async (year: number) => {
   try {
     const response = await axios.get(`${API_URL}${year}`);
